bc9b501550621436c2b41d1e97f3754a8970aa3d
earvinlin(off_vmwin8) <lin.earvin@gmail.com(off_vmwin8)>
2022-06-21 14:01:01 +0800

    20220621-1400 modify program.

--- AI_CH17_001.py
+++ AI_CH17_001.py
@@ -1,3 +1,4 @@
+"""
 import pandas as pd
 import statsmodels.stats.anova as anova
 from statsmodels.formula.api import ols
@@ -7,3 +8,23 @@ year_return = pd.read_csv('TRD_Year.csv', encoding = 'big5')
 model = ols('Return ~ C(Industry)', data = year_return.dropna()).fit()
 table1 = anova.anova_lm(model)
 print(table1)
+"""
+
+import pandas as pd
+import numpy as np
+TRD_Index = pd.read_table('index.csv', sep = '\t')
+TRD_Index.index = pd.to_datetime(TRD_Index.Date)
+Taiex = TRD_Index[TRD_Index.CoName == 'TSE Taiex    ']
+tw50 = TRD_Index[TRD_Index.CoName == 'TW 50 INDEX  ']
+#print(Taiex)
+#print(tw50)
+#print(pd.concat([Taiex.ROI, tw50.ROI], axis = 1))
+
+retData = pd.concat([Taiex.ROI, tw50.ROI], axis = 1).astype(np.float64)
+retData = retData.dropna()
+retData.columns = ['TAIEX', 'TW50']
+print(retData)
+
+import statsmodels.api as sm
+model = sm.OLS(retData.TAIEX, sm.add_constant(retData.TW50)).fit()
+print(model.summary())
