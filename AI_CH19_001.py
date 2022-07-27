4b2db369d549622e96b69e8d790a7ef9e7e0c5e6
Earvin Lin(SG3) <lin.earvin@gmail.com(SG3)>
2022-07-27 20:41:41 +0800

    20220727-2041 Add files.

--- AI_CH19_001.py
+++ AI_CH19_001.py
@@ -3,16 +3,32 @@ import numpy as np
 
 stock = pd.read_csv('2330.csv', index_col = 'Date', sep = '\t')
 close = stock.Close
-print(close.head())
+#print(close.head())
+print(close)
 
 close.index = pd.to_datetime(close.index)
 lagclose = close.shift(1)
 # 計算單期簡單收益率
 simpleret = (close - lagclose) / lagclose
 simpleret.name = 'simpleret'
-print(simpleret.head())
+#print(simpleret.head())
+print(simpleret)
 
+'''
 # 計算2期簡單收益率
 simpleret2 = (close -close.shift(2)) / close.shift(2)
 simpleret2.name = 'simpleret2'
 print(simpleret2.head())
+'''
+
+annualize = (1+simpleret).cumprod()
+print("== Test cumprod() ==")
+print(annualize)
+
+annualize = (1+simpleret).cumprod()[-1]
+print("== Test cumprod()[-1] ==")
+print(annualize)
+
+annualize = (1+simpleret).cumprod()[-1]**(245/311)-1
+print("== Test cumprod()[-1]**(245/311)-1 ==")
+print(annualize)
